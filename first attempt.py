dada6c6e85dc89b3c3d6e2fae551e6ebc46e43b8
Deatheater2020 <60698486+Deatheater2020@users.noreply.github.com>
2020-02-05 13:04:55 +0000

    update first attempt.py

--- first attempt.py
+++ first attempt.py
@@ -1,12 +1,42 @@
 import time
-print(hello)
 time.sleep(1)
-print(hello)
+print('hello')
 time.sleep(1)
-print(goodbye)
-
+print('hello')
+time.sleep(1)
+print('goodbye')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('goodbye')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('goodbye')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('hello')
+time.sleep(1)
+print('goodbye')
+time.sleep(0.5)
+print('this was a mistak writing this file as firts')
 
 a = 33
 b = 200
 if b > a:
   print("b is greater than a")
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple","supppppppppppppppppppppppppppppppsssssssssssss"]
+print(thislist)
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset) 
+thisset = {"apple", "banana", "cherry"}
+print(thisset) 
